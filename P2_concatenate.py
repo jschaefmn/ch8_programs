50f4eaa426f1170cf0c3e0da988bed4323b62de5
jschaefMB <jschaefmn@gmail.com>
2024-02-01 16:34:53 -0600

    finished P2

---
+++ P2_concatenate.py
@@ -0,0 +1,11 @@
+# This program concatenates strings
+def main():
+  name = 'Carmen'
+  print(f'The name is {name}')
+  
+  name = name + ' Brown'
+  print(f'Now the name is {name}')
+  
+# call main function
+if __name__ == '__main__':
+  main()
